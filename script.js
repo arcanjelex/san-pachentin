70c719e29c216a8395a2c91e1798b36aa19029c6
EstebanLool1 <156596922+arcanjelex@users.noreply.github.com>
2025-10-04 11:58:36 -0500

    terminado son goku

---
+++ script.js
@@ -0,0 +1,45 @@
+const btnAbrir =document.querySelector("#Abrir");
+const btnCerrar =document.querySelector("#Cerrar");
+
+//Abrir Carta
+btnAbrir.addEventListener("click", () => {
+    const ElementoSuperior = document.querySelector(".superior");
+    ElementoSuperior.classList.add("abrir-superior");
+
+    const h1 = document.querySelector("h1");
+    const p = document.querySelector("p");
+    h1.style.transform = "translateY(-120px)";
+    p.style.transform = "translateY(-120px)";
+    h1.style.transition = "transform 0.65s ease-in-out";
+    p.style.transition = "transform 0.65s ease-in-out";
+
+    const IconoEstrella = document.querySelector(".bx");
+    IconoEstrella.classList.add("bx.rotada");
+
+    setTimeout(() => {
+        ElementoSuperior.style.zIndex = -1;
+        const ElementoMensaje = document.querySelector(".mensaje");
+        ElementoMensaje.classList.add("abrir-mensaje");
+    }, 700);
+});
+
+//Cerrar Carta
+btnCerrar.addEventListener("click", () => {
+    const ElementoSuperior = document.querySelector(".superior");
+    const ElementoMensaje = document.querySelector(".mensaje");
+    ElementoMensaje.classList.remove("abrir-mensaje");
+    
+    setTimeout(() => {
+        const h1 = document.querySelector("h1");
+        const p = document.querySelector("p");
+        h1.style.transform = "translateY(0px)";
+        p.style.transform = "translateY(0px)";
+
+        ElementoSuperior.style.zIndex = 0;
+        ElementoSuperior.classList.remove("abrir-superior");
+    }, 700);
+
+
+    const IconoEstrella = document.querySelector(".bx");
+    IconoEstrella.classList.remove(".bx-rotada");
+})
